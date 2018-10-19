de67cf084f1627d1aaae2090ffb3726732cf3328
Ryan Stoddard <rstodd@uw.edu>
2018-10-19 11:13:55 -0700

    added some PL example use cases

---
+++ pvtools/PL/2018_10-19_PL_analysis.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 09:08:27 2018
+
+@author: ryanstoddard
+"""
+#%%
+#This section is just standard imports, defining default plot settings,
+# and defining constants
+#standard imports
+import numpy as np
+import pandas as pd
+import math
+import scipy
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
+import time
+import matplotlib.cm as cm
+import matplotlib as mpl
+from os import listdir
+
+import sys
+sys.path.append('../../')
+from pvtools.PL import PLtools
+
+
+#change default plot settings
+default_figsize = mpl.rcParamsDefault['figure.figsize']
+mpl.rcParams['figure.figsize'] = [1.5*val for val in default_figsize]
+font = {'family' : 'DejaVu Sans',
+        'weight' : 'bold',
+        'size'   : 24}
+
+mpl.rc('font', **font)
+mpl.rc('axes', linewidth=3)
+
+#Constants
+pi = math.pi
+heV = 4.14e-15 #eV*s
+c = 2.99792e8 #m/s
+kbeV = 8.6173e-5 #eV/K
+keV = 8.6173e-5 #eV/K
+h = 6.626e-34
+kb = 1.38065e-23
+q = 1.60218e-19
+
+#%%
+#This section we define inputs 
+#Inputs Here
+directory = '2018_10-12PL'
+laser_power = 32*1e-9
+grating = '500nm' # '500nm' or '1200nm'
+#dname = 'Dark.txt' #assumes 1 dark name per folder
+
+#Now lets look into the directory to see what files we have to analyze
+#(this avoids having to copy/paste filenames)
+names = []
+for file in listdir(directory):
+    if file[-6:] == 'XY.txt':
+        names.append(file)
+    elif file[-8:] == 'Dark.txt':
+        dname = file
+        
+print(names)
+print(dname)
+
+#%%
+#Use Case 1: We want to took at a few PL maps, convert to absolute units,
+#then plot 1 spectrum per map on same plot. 
+for ii in (7, 2, 1, 0):
+    #Load data, using pd. Can also use np.loadtxt, but less flexible
+    df = pd.read_table(directory + '/' + dname,header=None)
+    dark = df.values    
+    df = pd.read_table(directory + '/' + names[ii],header=None)
+    data = df.values
+    
+    #Convert to aboslute units with single line of code, calling function from
+    #PLtools module
+    aipl_data = PLtools.aipl(data,dark,grating)
+    
+    #Lets choose the peak with median PLQY. There is a function in PLtools 
+    #that returns index to median PLQY spectrum
+    idx = PLtools.med_idx(aipl_data)
+    
+    #Look at map data to see where AIPL info is, and also to find E
+    k = 0
+    while np.isnan(aipl_data[0,k]):
+        k = k + 1
+    lam = aipl_data[0,k:]
+    E = heV*c/(lam*1e-9)
+    spectrum = aipl_data[idx,k:]
+    
+    #Now prepare plot
+    plt.plot(E,spectrum,'-',linewidth=3)
+plt.xlabel('$E\ [eV]$')
+plt.ylabel('$I_{PL}\ [photons/m^2*sec*eV]$')
+plt.legend(['FAGACs','+PEAI','+TOPO','+Both'])
+
+#%%
+#Use Case 2: Lets look at maps, calculate QFLS and chi with PLQY method,
+#then plot distributions
+
+#Pre-allocate lists, we will use these to plot
+meanPL_list = []
+PLQY_list = []
+QFLS_list = []
+chi_list = []
+for ii in (7, 2, 1, 0):
+    #Load data, using pd. Can also use np.loadtxt, but less flexible
+    df = pd.read_table(directory + '/' + dname,header=None)
+    dark = df.values    
+    df = pd.read_table(directory + '/' + names[ii],header=None)
+    data = df.values
+    
+    #Convert to aboslute units with single line of code, calling function from
+    #PLtools module
+    aipl_data = PLtools.aipl(data,dark,grating)
+    
+    #Determine PL params using PLtools.plqy_ext function
+    (mean_Ipl, peak_pos, FWHM, 
+     PLQY, dmu_PLQY, chi_PLQY,
+     dmu_PLQY_Eg, chi_PLQY_Eg) = PLtools.plqy_ext(aipl_data, laser_power)
+    
+    #Look at map data to see where AIPL info is, and also to find E
+    k = 0
+    while np.isnan(aipl_data[0,k]):
+        k = k + 1
+    lam = aipl_data[0,k:]
+    E = heV*c/(lam*1e-9)
+    spectrum = aipl_data[idx,k:]
+    
+    #Now prepare plot
+    plt.plot(E,spectrum,'-',linewidth=3)
+plt.xlabel('$E\ [eV]$')
+plt.ylabel('$I_{PL}\ [photons/m^2*sec*eV]$')
+plt.legend(['FAGACs','+PEAI','+TOPO','+Both'])
